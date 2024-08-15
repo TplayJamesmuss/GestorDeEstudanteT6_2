9d6caa7043dc5e018cccd43c16f08adb90e0fe66
Thiago Mussolino <167465792+TplayJamesmuss@users.noreply.github.com>
2024-08-15 15:22:52 -0300

    Configuração do ButtonBuscar

--- GestorDeEstudantesT6/FormAtualizarApagarAlunos.cs
+++ GestorDeEstudantesT6/FormAtualizarApagarAlunos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -139,7 +140,13 @@ namespace GestorDeEstudantesT6
 
         private void buttonBuscar_Click(object sender, EventArgs e)
         {
+            // Busca estudante pela ID.
+            // Já salva a ID convertida para INTEIRO.
+            int id = Convert.ToInt32(textBoxId.Text);
+            MeuBancoDeDados meuBancoDeDados = new MeuBancoDeDados();
 
+            MySqlCommand comando = new MySqlCommand("SELECT `id`,`nome`,`sobrenome`,`nascimento`,`genero`,`telefone`,`endereco`,`foto` FROM `estudantes` WHERE `id`=" + id,
+                meuBancoDeDados.getConexao);
         }
 
         bool Verificar()
